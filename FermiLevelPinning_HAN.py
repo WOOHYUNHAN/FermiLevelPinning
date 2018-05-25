b0f9e0f6ad7eb2350611dab38cd7e97530f4a62c
WOOHYUNHAN <hanwooh@gmail.com>
2018-05-25 17:25:07 +0900

    code up

--- FermiLevelPinning_HAN.py
+++ FermiLevelPinning_HAN.py
@@ -15,6 +15,7 @@ class Make_defective_model:
         self.vbm = vbm  # vbm should be absolute value from DFT calculations not ralative values as compared to vbm, Fermi level ...
         self.cbm = cbm  # cbm should be absolute value from DFT calculations not ralative values as compared to vbm, Fermi level ...
         self.band_gap = cbm - vbm
+        self.intrinsic_defect_info = []
 
     def set_bulk_property(self, Etotal, DOS_info, additional_info):
         self.E_bulk = Etotal
@@ -56,8 +57,23 @@ class Make_defective_model:
 
         return 0
 
-    def set_intrinsic_defect(self, defect_name, E_defect, chemical_potential):
-        
+    def set_intrinsic_defect(self, defect_name, E_defect, charge_state, chemical_potential, create_anhil_info):
+        '''
+        defect formation energy:
+        '''
+        if len(chemical_potential) != len(create_anhil_info):
+            print 'ERROR: please check both chemical potential and crate_anhil_info'
+            return 0
+
+        chemical_info = np.array(chemical_info) * np.array(create_anhil_info)
+        charge_info = charge_state * self.vbm
+
+        diff_energy = E_defect - self.E_bulk - chemical_info - charge_info
+
+
+
+
+
 
     def draw_thermodynamic_charge_transition_level(self):
         return 0
